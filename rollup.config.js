67d160dec3ff4164ee190a22477ac0892addab40
Aleksandr Tsertkov <tsertkov@gmail.com>
2019-12-29 20:50:44 +0100

    Add rollup manifest

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const outputManifest = require('rollup-plugin-output-manifest').default
 
 const src = path.join(__dirname, 'src')
 
@@ -6,6 +7,12 @@ export default {
   input: path.join(src, 'scripts', 'main.js'),
   output: {
     file: path.join(src, 'public', 'scripts', 'main.rollup.js'),
-    format: 'cjs'
-  }
+    format: 'iife'
+  },
+  plugins: [
+    outputManifest({
+      publicPath: '/scripts/',
+      fileName: path.join(src, 'data-static', 'rollup-manifest.json'),
+    })
+  ]
 }
